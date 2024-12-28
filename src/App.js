cfc9ce3fe7f055a7d8f6adbdaa8860c96e402550
Farbod Kaymasi <Farbodkimasy@gmail.com>
2024-12-28 19:40:51 +0330

    Added redirect for /FlightManagementApp to root

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import { Provider } from "react-redux"; // Import Provider from react-redux
 import store from "./store"; // Import the Redux store
-import Home from './pages/Home'
+import Home from "./pages/Home";
 import AddFlight from "./pages/addFlight";
 import FlightsList from "./pages/flightList";
 import UpdateFlight from "./pages/updateFlight";
@@ -10,21 +15,33 @@ import FlightsByDestination from "./pages/flightsByDestination";
 import FlightWithMostPassengers from "./pages/flightsWithMostPassenger";
 import DeleteFlight from "./pages/deleteFlight";
 import Menu from "./pages/Menu";
-import './App.css'; 
+import "./App.css";
 
 const App = () => {
   return (
-
     <Provider store={store}>
       <Router>
         <Menu />
         <Routes>
+          {/* Redirect from /FlightManagementApp to root */}
+          <Route
+            path="/FlightManagementApp"
+            element={<Navigate to="/" replace />}
+          />
+
+          {/* Other routes */}
           <Route path="/" element={<Home />} />
           <Route path="/add-flight" element={<AddFlight />} />
           <Route path="/flights-list" element={<FlightsList />} />
           <Route path="/update-flight" element={<UpdateFlight />} />
-          <Route path="/flights-by-destination" element={<FlightsByDestination />} />
-          <Route path="/most-passengers" element={<FlightWithMostPassengers />} />
+          <Route
+            path="/flights-by-destination"
+            element={<FlightsByDestination />}
+          />
+          <Route
+            path="/most-passengers"
+            element={<FlightWithMostPassengers />}
+          />
           <Route path="/delete-flight" element={<DeleteFlight />} />
         </Routes>
       </Router>
